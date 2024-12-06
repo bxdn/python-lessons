16a31ff6eae8af8f2f713387fe4d3d92c5782cfb
bxdn <33440510+bxdn@users.noreply.github.com>
2024-12-06 09:51:37 -0500

    Fix game description
    Co-authored-by: SopraninoB <SopraninoB@users.noreply.github.com>

--- game.py
+++ game.py
@@ -4,7 +4,7 @@
 # This will continue until the guess is correct, when the game will let the user know they've won, and then exit.
 
 # Part 1: Create a secret number in the code between 1 and 20 (inclusive).  Prompt the user for a guess, and then print whether user's guess and the actual secret number are the same.
-# Part 2: Alter Part 1 to print whether the user's guess is higher, lower, or the same as the secret number instead of the actual numbers and difference.
+# Part 2: Alter Part 1 to print whether the user's guess is higher, lower, or the same as the secret number instead of whether it's equal
 # Part 3: Alter Part 2 to repeat until the user's guess is the same as the secret number, then congratulate the user, and exit.
 # Part 4: Alter Part 3 to use a random number between 1 and 20 (inclusive) instead of a entering it into the code manually.
 
